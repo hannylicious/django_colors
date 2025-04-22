d5bbd7f7203ed86fc27b325d31b644396759771b
AoS <aos.fontana.dev@gmail.com>
2025-04-22 14:32:51 -0300

    fields update
    formfield refactor

--- colors/fields.py
+++ colors/fields.py
@@ -2,16 +2,15 @@ from django.db.models.fields import CharField
 from django.db.models.base import Model
 from django.db.models.query import QuerySet
 from django.forms import ChoiceField
-from django.db.models.functions import Lower
 from django.utils.translation import gettext as _
 from .widgets import ColorChoiceWidget
 from .color_definitions import ColorChoices, BootstrapColorChoices
-
+from .field_type import FieldType
 
 class ColorModelField(CharField):
     choice_model: Model
     choice_queryset: QuerySet
-    value_attribute: str
+    value_attribute: FieldType
     default_options: ColorChoices
     description = _("String for use with css (up to %(max_length)s)")
 
@@ -19,7 +18,7 @@ class ColorModelField(CharField):
         self,
         model=None,
         queryset=None,
-        value_attribute=None,
+        value_attribute=FieldType.BACKGROUND,
         default_options=BootstrapColorChoices,
         *args,
         **kwargs,
@@ -27,8 +26,9 @@ class ColorModelField(CharField):
         self.choice_model = model
         self.choice_queryset = queryset
         self.value_attribute = value_attribute
-        self.default_options = default_options
+        self.default_options = default_options()
         kwargs.setdefault("max_length", 150)
+
         super().__init__(*args, **kwargs)
 
     @property
@@ -41,38 +41,42 @@ class ColorModelField(CharField):
         )
 
     def deconstruct(self):
-        # modified deconstruct method to properly add args (model or queryset)
-        # in migration field definition.
         name, path, args, kwargs = super().deconstruct()
+        if self.value_attribute:
+            kwargs["value_attribute"] = self.value_attribute
         if self.choice_model:
             kwargs["model"] = self.choice_model
         if self.choice_queryset:
             kwargs["queryset"] = self.choice_queryset
-        if self.value_attribute:
-            kwargs["value_attribute"] = self.value_attribute
-        kwargs["default_options"] = self.default_options
-
         return name, path, args, kwargs
 
     def formfield(self, **kwargs):
+
         """Creates a forms.ChoiceField with a custom widget and choices."""
 
-        choices = self.default_options.choices  # default colors
+        def get_choices():
+            choices = list(self.default_options.choices)
 
-        if self.choice_queryset is not None:
-            choices.extend(
-                list(self.choice_queryset.values_list("name", self.value_attribute))
-            )
-
-        elif self.choice_model is not None:
-            choices.extend(
-                list(
-                    self.choice_model.objects.all().values_list(
-                        "name", self.value_attribute
-                    )
+            if self.choice_queryset is not None:
+                choices.extend(
+                    list(self.choice_queryset.values_list("name", self.value_attribute.value))
                 )
-            )
 
-        kwargs["widget"] = ColorChoiceWidget(choices=choices)
+            elif self.choice_model is not None:
+                try:
+                    choices.extend(
+                        list(
+                            self.choice_model.objects.all().values_list(
+                                "name", self.value_attribute.value
+                            )
+                        )
+                    )
+                except Exception:
+                    #avoid the model not being ready yet in migrations
+                    pass
 
-        return ChoiceField(choices=choices, **kwargs)
+            return choices
+
+        kwargs["widget"] = ColorChoiceWidget
+        return ChoiceField(choices=get_choices, **kwargs)
+
